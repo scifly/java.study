efd691de451a96a596a9a064df0232f8f1f3ed7a
scifly <haoyh@yahoo.com>
2020-11-22 15:30:59 +0800

    collection
    Java collection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,26 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-	    // write your code here
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
+        Scanner in = new Scanner(new File("test.txt"));
+        List<String> students = new ArrayList<String>();
 
-        System.out.println("You entered: " + s);
+        while (in.hasNextLine()) {
+            students.add(in.nextLine());
+        }
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println("name: " + students.get(i));
+        }
+
+        in.close();
 
     }
 }
