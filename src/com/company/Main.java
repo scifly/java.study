f1baf17d5b148efc119a74175b3ec6892c8a75cb
scifly <haoyh@yahoo.com>
2020-11-22 14:23:22 +0800

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        System.out.println("You entered: " + s);
+
+    }
+}
