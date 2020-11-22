5d7bdc55fda54b42ad67950f2bd5f3857dc940e1
scifly <haoyh@yahoo.com>
2020-11-22 14:23:56 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    // write your code here
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
 
